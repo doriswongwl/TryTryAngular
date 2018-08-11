747906ecc83ba6c77377009758fe789f34571b94
Doris Wong <doriswongwl@gmail.com>
2018-08-12 00:23:56 +0800

    title changed

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Toure of Heroesssss';
+  title = 'Toure of Heroesssss11';
 }
