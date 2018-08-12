d5fe325f59bd4524398ffde976012ab9b45d6a53
doriswongwl <37473867+doriswongwl@users.noreply.github.com>
2018-08-12 08:59:16 +0800

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Toure of Heroesssss11ABCDEFG';
+  title = 'Toure of Heroes, HIHI';
 }
